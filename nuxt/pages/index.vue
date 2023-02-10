dd4f6db24b72e98e68061f25d1a300d607f70187
huamurui <yuntai013@outlook.com>
2023-02-11 01:04:05 +0800

    😉try middleware and jwt and so on

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
 
   <NuxtLink to="/juejin">Juejin Clone</NuxtLink> <a>← click here...比较惨不忍睹...可以的话，自己把样式重写一遍也可以</a>
+
+  <div @click="onSubmit">login test</div>
+
   <NuxtLayout name="nondefault">
     <template #header>
       this is header
@@ -12,10 +15,42 @@
       this is aside
     </template>
   </NuxtLayout>
+
+  <JuejinDialogSign />
 </template>
 
 
 <script setup lang="ts">
+
+
+
+const { login } = useStrapiAuth()
+
+const onSubmit = async () => {
+  try {
+    let a = await login({ identifier: 'mua', password: 'muamua' })
+
+    console.log(a)
+
+    // 如果登录成功，这里的 a 里面就有 jwt。
+    // 要存到 localStorage 里面，然后在一些需要权限的请求的时候用一下。建议是不要用cookie存这个jwt，因为cookie现在安全有问题...而session貌似要放后端。所以最后就是localStorage。
+
+    // 而说到 localStorage... 用户选择的主题模式也是需要存一下的，但我现在也还是没写。
+  } catch (e) { }
+}
+
+// dialog form vue3
+// https://dev.to/garmideroman/the-simplest-way-to-deal-with-modal-dialogs-in-vue-3-59hl
+
+
+// const { reveal, onConfirm } = createConfirmDialog(JuejinSign, { question: 'Are you sure you want to hide the logo?' })
+
+// onConfirm(() => {
+//   showLogo.value = false
+// })
+
+
+
 definePageMeta({
   layout: false
 });
